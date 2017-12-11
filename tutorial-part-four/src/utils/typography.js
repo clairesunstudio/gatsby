7798f2f6fbd8e89a496bf783a7ee8ef1de590f8b
Minghua Sun <clairesunstudio@gmail.com>
2017-12-11 16:47:42 -0500

    set up part 4

---
+++ tutorial-part-four/src/utils/typography.js
@@ -0,0 +1,7 @@
+import Typography from "typography";
+import kirkhamTheme from "typography-theme-kirkham";
+
+kirkhamTheme.baseFontSize = '16px'
+const typography = new Typography(kirkhamTheme);
+
+module.exports = typography;
